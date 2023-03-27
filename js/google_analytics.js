700b6c0d8946c6d12874a89dd561bfccf90d8d28
yuchin <chin.frontend@gmail.com>
2023-03-27 22:18:05 +0800

    Site updated: 2023-03-27 22:18:04

---
+++ js/google_analytics.js
@@ -0,0 +1,6 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QC5222B50L"></script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-QC5222B50L');
